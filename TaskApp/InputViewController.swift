0410fa7ad8c97bf3f3684a3baf72974526ccd10a
nako-ay <a.y.1125.ami@gmail.com>
2025-08-19 01:22:46 +0900

    Initial Commit pod installとgitignoreの作成

---
+++ TaskApp/InputViewController.swift
@@ -0,0 +1,32 @@
+//
+//  InputViewController.swift
+//  TaskApp
+//
+//  Created by haiden2 on 2025/08/24.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var titleTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by haiden2 on 2025/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tableView.fillerRowHeight = UITableView.automaticDimension
+    }
+    
+    // 各セルを選択した時に実行されるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue",sender: nil)
+    }
+}
+
